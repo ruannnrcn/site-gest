a10e7e811836324185e055671d209d251da39aeb
Pedro Ruan <109441047+ruannnrcn@users.noreply.github.com>
2023-09-08 11:03:33 -0300

    Add files via upload

---
+++ src/public/scripts/Script_DarkMode.js
@@ -0,0 +1,13 @@
+let main = document.querySelector("main");
+
+function darkmode() {
+  main.classList.toggle("dark");
+}
+
+// let body=document.querySelector("body")
+// let h1 = document.querySelector("h1")
+// let div = document.querySelector("div")
+// let h2 = document.querySelector ("h2")
+// h2.addEventListener("mousedown",() => {
+//     body.classList.toggle("dark")
+// })
